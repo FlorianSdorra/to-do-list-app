8d2eba0d18819b15affa1db95aef1e579ae862f2
FlorianSdorra <fsd114@posteo.de>
2019-12-15 15:32:16 +0100

    created controller for tasks

---
+++ controllers/tasksController.js
@@ -0,0 +1,23 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('data/db.json');
+const db = low(adapter);
+
+
+exports.getTasks = async (req, res, next) => {
+    const tasks = db.get('tasks').value()
+    res.status(200).send(tasks);
+}
+
+
+exports.addTask = async (req, res, next) => {
+    const task = req.body;
+    db.get('records').push(task)
+        .last()
+        .assign({
+            id: Date.now().toString()
+        })
+        .write()
+
+    res.status(200).send(task);
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,31 +4,20 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('data/db.json');
 const db = low(adapter);
+const {
+    getTasks,
+    addTask
+} = require('../controllers/tasksController');
 
 /**
  * GET all tasks
  */
-router.get('/', function (req, res, next) {
-    const tasks = db.get('tasks')
-        .value()
-
-    res.status(200).send(tasks)
-});
+router.get('/', getTasks);
 
 
 /**
  * POST a task
  */
-router.post('/', function (req, res, next) {
-    let task = req.body;
-    db.get('tasks').push(task)
-        .last()
-        .assign({
-            id: Date.now().toString()
-        })
-        .write()
-
-    res.status(200).send(task);
-});
+router.post('/', addTask);
 
 module.exports = router;
